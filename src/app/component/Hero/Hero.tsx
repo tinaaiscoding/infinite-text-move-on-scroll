e87a6baed85678041d5ca484892bd4913740c559
tinanvo <tinanhivo@gmail.com>
2025-07-15 20:41:06 +1000

    Marquee animation

--- src/app/component/Hero/Hero.tsx
+++ src/app/component/Hero/Hero.tsx
@@ -1,25 +1,49 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
 
 import './Hero.css';
 
 export default function Hero() {
+  const firstText = useRef<HTMLHeadingElement>(null);
+  const secondText = useRef<HTMLHeadingElement>(null);
+
+  let xPercent = 0;
+
+  useEffect(() => {
+    if (!firstText.current) return;
+
+    gsap.set(secondText.current, {
+      left: firstText.current?.getBoundingClientRect().width,
+    });
+
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    if (xPercent <= -100) {
+      xPercent = 0;
+    }
+
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+
+    requestAnimationFrame(animate);
+
+    xPercent -= 0.1;
+  };
+
   return (
     <main className='hero_wrap flex items-end'>
-      <motion.div
-        className='hero_slider'
-        initial={{ x: 0 }}
-        animate={{ x: '-100%' }}
-        transition={{
-          duration: 5,
-          ease: 'linear',
-          repeat: Infinity,
-          repeatType: 'loop',
-        }}
-      >
-        <h1 className='hero_heading'>TRANSFORM • ALIGN • FUTURISTIC •</h1>
-      </motion.div>
+      <div className='hero_slider'>
+        <h2 ref={firstText} className='hero_heading'>
+          TRANSFORM • ALIGN • FUTURISTIC •
+        </h2>
+        <h2 ref={secondText} className='hero_heading'>
+          TRANSFORM • ALIGN • FUTURISTIC •
+        </h2>
+      </div>
     </main>
   );
 }
