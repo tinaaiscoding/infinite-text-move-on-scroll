904663acb08c398727a2bde85cd942f48c452ef5
tinanvo <tinanhivo@gmail.com>
2025-07-16 20:43:20 +1000

    Add scroll effect

--- src/app/component/Hero/Hero.tsx
+++ src/app/component/Hero/Hero.tsx
@@ -1,15 +1,20 @@
 'use client';
 
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useEffect, useRef } from 'react';
 
 import './Hero.css';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Hero() {
   const firstText = useRef<HTMLHeadingElement>(null);
   const secondText = useRef<HTMLHeadingElement>(null);
+  const slider = useRef<HTMLDivElement>(null);
 
   let xPercent = 0;
+  let direction = -1;
 
   useEffect(() => {
     if (!firstText.current) return;
@@ -19,11 +24,27 @@ export default function Hero() {
     });
 
     requestAnimationFrame(animate);
+
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: window.innerHeight,
+        scrub: 0.25,
+        onUpdate: (e) => {
+          direction = e.direction * -1;
+        },
+      },
+      x: '-=500px',
+    });
   }, []);
 
   const animate = () => {
     if (xPercent <= -100) {
       xPercent = 0;
+    }
+    if (xPercent > 0) {
+      xPercent = -100;
     }
 
     gsap.set(firstText.current, { xPercent: xPercent });
@@ -31,12 +52,12 @@ export default function Hero() {
 
     requestAnimationFrame(animate);
 
-    xPercent -= 0.1;
+    xPercent += 0.1 * direction;
   };
 
   return (
     <main className='hero_wrap flex items-end'>
-      <div className='hero_slider'>
+      <div ref={slider} className='hero_slider'>
         <h2 ref={firstText} className='hero_heading'>
           TRANSFORM • ALIGN • FUTURISTIC •
         </h2>
